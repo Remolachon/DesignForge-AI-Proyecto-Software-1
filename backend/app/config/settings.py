2f28d79231964c4ca8416d6b56bbdfd2b3298db9
DuvanPerez03 <duvan.perez03@uptc.edu.co>
2026-05-10 01:01:03 -0500

    Integracion de IA y FallBacks de procesamiento de imagenes

--- backend/app/config/settings.py
+++ backend/app/config/settings.py
@@ -1,16 +1,26 @@
-from pydantic_settings import BaseSettings
+import os
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
 
 class Settings(BaseSettings):
-    DATABASE_URL: str 
-    SUPABASE_URL: str 
-    SUPABASE_KEY: str 
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="allow"  # permite variables extras en .env
+    )
+
+    DATABASE_URL: str
+    SUPABASE_URL: str
+    SUPABASE_KEY: str
 
     JWT_SECRET: str | None = None
     EMAIL_HOST: str | None = None
     EMAIL_USER: str | None = None
     EMAIL_PASSWORD: str | None = None
-    AI_API_KEY: str | None = None
-    
+    HF_TOKEN: str
+    HF_SPACE_ID: str = "Dupan21/LukArt"
+
     # PayU Configuration
     PAYU_MERCHANT_ID: str | None = None
     PAYU_ACCOUNT_ID: str | None = None
@@ -25,7 +35,5 @@ class Settings(BaseSettings):
     GOOGLE_CLIENT_SECRET: str | None = None
     GOOGLE_REDIRECT_URI: str | None = None
 
-    class Config:
-        env_file = ".env"
 
 settings = Settings()
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,14 +1,23 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import app.models 
 
+import app.models
 from app.controllers.user_controller import router as user_router
 from app.controllers.auth_controller import router as auth_router
 from app.controllers import product_controller
 from app.controllers import order_controller
 from app.controllers import upload_controller
+from contextlib import asynccontextmanager
 
-app = FastAPI(title="Embroidery Marketplace API")
+from app.controllers import ai_controller
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
+
+app = FastAPI(lifespan=lifespan)
+app = FastAPI(title="Embroidery Marketplace API", lifespan=lifespan)
 
 origins = [
     "http://localhost:3000",
@@ -17,18 +26,19 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# RUTAS
 app.include_router(user_router)
 app.include_router(auth_router)
 app.include_router(product_controller.router)
 app.include_router(order_controller.router)
 app.include_router(upload_controller.router)
+app.include_router(ai_controller.router)
+
 
 @app.get("/")
 def root():
